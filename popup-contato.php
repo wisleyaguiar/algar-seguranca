17c17a3d090810c4bcfd58cd7d303e3dbbd05441
wisleyaguiar <wisleyaguiar@hotmail.com>
2015-09-17 10:45:20 -0300

    Ultima atualizacao

--- popup-contato.php
+++ popup-contato.php
@@ -1,5 +1,5 @@
 <div class="popup-contato">
     <!--a href="#" class="bt-fechar-contato">Fechar</a-->
-    <div class="num-tel">Entre em contato<br><span><?php echo get_option('tel_box_contato'); ?>2</span></div>
+    <div class="num-tel">Entre em contato<br><span><?php echo get_option('tel_box_contato'); ?></span></div>
     <button type="submit" name="receberContato" id="receberContato" class="bt-receber-contato" onClick="window.location='<?php echo home_url() ?>/contato'; dataLayer.push('receberContato','<?php echo get_the_ID(); ?>');">Receber contato</button>
 </div>
